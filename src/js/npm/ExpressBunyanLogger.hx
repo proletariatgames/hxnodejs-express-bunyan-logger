dc66778b4315f5a226585b1a3fe2d452e6478ccb
Dan Brakeley <dan@brakeley.net>
2016-05-06 15:52:26 -0400

    missing semicolons

--- src/js/npm/ExpressBunyanLogger.hx
+++ src/js/npm/ExpressBunyanLogger.hx
@@ -3,15 +3,15 @@ package js.npm;
 import express.*;
 
 typedef Options = {
-  @:optional var excludes : Array<String>
-  @:optional var format : String
-  @:optional var genReqId : Request -> String
-  @:optional var immediate : Bool
+  @:optional var excludes : Array<String>;
+  @:optional var format : String;
+  @:optional var genReqId : Request -> String;
+  @:optional var immediate : Bool;
   @:optional var includesFn : Request -> Response -> Dynamic;
   // TODO: @:optional var levelFn: ???
   // TODO: @:optional var logger : ???
-  @:optional var name : String
-  @:optional var parseUA : Bool
+  @:optional var name : String;
+  @:optional var parseUA : Bool;
   // TODO: @:optional var serializers : ???
 }
 
