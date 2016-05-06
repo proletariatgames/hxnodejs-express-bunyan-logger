736fa15612459f4a612ee0e879114dccb1b325f6
Dan Brakeley <dan@brakeley.net>
2016-05-06 15:49:51 -0400

    add additional options

--- src/js/npm/ExpressBunyanLogger.hx
+++ src/js/npm/ExpressBunyanLogger.hx
@@ -3,7 +3,16 @@ package js.npm;
 import express.*;
 
 typedef Options = {
+  @:optional var excludes : Array<String>
+  @:optional var format : String
+  @:optional var genReqId : Request -> String
+  @:optional var immediate : Bool
   @:optional var includesFn : Request -> Response -> Dynamic;
+  // TODO: @:optional var levelFn: ???
+  // TODO: @:optional var logger : ???
+  @:optional var name : String
+  @:optional var parseUA : Bool
+  // TODO: @:optional var serializers : ???
 }
 
 @:jsRequire("express-bunyan-logger")
